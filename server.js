f64f61a3f8b74ec6c447667e96a04e283817bbbe
IgnacioRV <nacho.rasche@gmail.com>
2016-08-20 11:04:36 +0200

    Added support for http

--- server.js
+++ server.js
@@ -32,6 +32,36 @@ app.get("/new/https://:url", (req,res)=>{
 
 	res.send(JSON.stringify(obj))
 })
+
+
+app.get("/new/http://:url", (req,res)=>{
+	console.log("ACCESSING LONG URL")
+	/*
+		We must add the url to the database and return a JSON with the new and old
+	1- Generate the number for the new url -> 2 ways: random or linear
+			 random is better for security, linear is easier to "hack"
+			 the umber could be stored in the db but that would add another access to the db, very expensive 
+	2- Add to the database the following: 2 keys element with "new" and "old" params
+	*/
+
+	var number = Math.round(Math.random()*90000)
+
+	var obj = {
+		"old": "http://"+req.params.url,
+		"new": "http://minifyer.herokuapp.com/"+number
+	}
+
+	mongo.connect (dbUrl, function (err, db){
+		if (err) throw err
+		var map = db.collection('map')
+		map.insert( obj, function (err, data){});
+		console.log("Inserted:")
+		console.log(obj)
+		db.close()
+	})
+
+	res.send(JSON.stringify(obj))
+})
 // TODO: The same but for http
 
 app.get("/favicon.ico", (req, res )=>{
