50bcc707377344192974c2223694dcd71a3a1bdc
IgnacioRV <nacho.rasche@gmail.com>
2016-08-20 09:31:52 +0200

    Added the db

--- server.js
+++ server.js
@@ -1,52 +1,65 @@
 var express = require('express')
 var app = express()
+var mongo = require('mongodb').MongoClient
+var dbUrl = process.env.MONGOLAB_URI
 app.set('port', (process.env.PORT || 5000));
 
 app.get("/new/https://:url", (req,res)=>{
+	console.log("ACCESSING LONG URL")
 	/*
 		We must add the url to the database and return a JSON with the new and old
-
-	TODO: 
-
 	1- Generate the number for the new url -> 2 ways: random or linear
 			 random is better for security, linear is easier to "hack"
 			 the umber could be stored in the db but that would add another access to the db, very expensive 
 	2- Add to the database the following: 2 keys element with "new" and "old" params
 	*/
 
-	var number = Math.round(Math.random()*9000)
+	var number = Math.round(Math.random()*90000)
 
 	var obj = {
 		"old": "https://"+req.params.url,
-		"new": "https://shielded-brushlands-21218.herokuapp.com/"+number
+		"new": "https://minifyer.herokuapp.com/"+number
 	}
+
+	mongo.connect (dbUrl, function (err, db){
+		if (err) throw err
+		var map = db.collection('map')
+		map.insert( obj, function (err, data){});
+		console.log("Inserted:")
+		console.log(obj)
+		db.close()
+	})
+
 	res.send(JSON.stringify(obj))
 })
 // TODO: The same but for http
 
+app.get("/favicon.ico", (req, res )=>{
+
+})
 
 app.get("/:short", (req,res)=>{
 /*
-Here we get the long url from the db and redirect to it		
-db.connect (function (err, db){
-	var map = db.collection("map")
-	map.find( 
-	{
-	 "old" : {$eq : req.params.short}
-	}).toString(function (documents){
-		callback(documents)
-	})	
+Here we get the long url from the db and redirect to it	
+*/	
+	console.log("ACCESSING SHORT URL")
+	console.log(req.params.short)
+	var newstring = "https://minifyer.herokuapp.com/"+req.params.short
+	mongo.connect (dbUrl, function (err, db){
+		if (err) throw err
+		var map = db.collection("map")
+
+		map.find( 
+		{
+		 "new" : newstring
+		}).toArray(function (err, documents){
+			console.log(documents)
+			res.redirect(documents[0].old)
+		})	
+		db.close()
+	})
 })
 
-*/
-	
-
-})
-
-function callback(documents){
-	var url = "https://"+documents;
-	// redirect to url
-}
 
 app.get("/", (req, res)=>{
 	/*
